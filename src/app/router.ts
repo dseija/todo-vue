408fd136aaf8161fb1f866ff41c5734f5ea157e4
David Seija <davidseija@gmail.com>
2023-03-03 17:32:31 -0500

    Add NotFound route & component

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
+import NotFound from '../pages/NotFound.vue';
 import Signin from '../pages/Signin.vue';
 import Signup from '../pages/Signup.vue';
 
 const appRouter = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/:pathMatch(.*)*', component: NotFound },
     { path: '/', component: Home },
     { path: '/signin/:action?/:status?', component: Signin, props: true },
     { path: '/signup', component: Signup },
---
+++ src/pages/NotFound.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>Aw, Snap!</h2>
+  The section you're looking for doesn't exist. Go to the
+  <router-link to="/">Home Section</router-link>!
+</template>
