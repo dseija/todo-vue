645ae325ede589fea8e15ff07de5122fc4d79410
David Seija <davidseija@gmail.com>
2023-03-05 21:24:59 -0500

    Add vuetify & theme settings implementation

--- src/features/settings/SettingsList.vue
+++ src/features/settings/SettingsList.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import { useTheme } from 'vuetify';
 import { saveSettings } from './settingsHelpers';
 import { updateSettings } from './settingsService';
 import useSettingsStore from './settingsStore';
 
+const theme = useTheme();
 const settingsStore = useSettingsStore();
 
 const toggleThemeMode = () => {
   settingsStore.toggleThemeMode();
+  theme.global.name.value = settingsStore.settings.themeMode;
   const saved = saveSettings(settingsStore.settings);
   if (saved) updateSettings(settingsStore.settings);
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import VueCookies from 'vue-cookies';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 import { appRouter } from './app';
 import { App } from './app';
+import 'vuetify/styles';
+import { loadSavedSettings } from './features/settings';
+
+const { themeMode } = loadSavedSettings();
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: themeMode,
+  },
+});
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -10,6 +24,7 @@ const app = createApp(App);
 app.use(VueCookies);
 app.use(pinia);
 app.use(appRouter);
+app.use(vuetify);
 app.mount('#app');
 
 const { $cookies } = app.config.globalProperties;
